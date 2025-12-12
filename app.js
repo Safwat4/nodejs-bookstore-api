8f3ef4c47ba22de64918d06a681b6efb73ec72a5
safwat <safwat.ahmed157@gmail.com>
2025-12-12 20:24:13 +0200

    Commit the modification on Database connection

--- app.js
+++ app.js
@@ -8,19 +8,21 @@ const bookRoutes = require('./Routers/books');
 const app = express();
 app.use(bodyParser.json());
 
-// MongoDB connection
-const uri = "mongodb+srv://safwatahmed:safwat123@sadb.1bj2yxp.mongodb.net/?appName=SADB";
+const uri = process.env.MONGO_URI;
+
 const connectDB = async () => {
     try {
         mongoose.set('strictQuery', false);
-        mongoose.connect(uri);
+        await mongoose.connect(uri);
         console.log("MongoDB connected");
     } catch (err) {
         console.error("Failed to connect to MongoDB", err);
-        process.exit();
+        process.exit(1);
     }
 };
+
 connectDB();
+
 
 app.use('/', userRoutes);
 app.use('/', bookRoutes);
